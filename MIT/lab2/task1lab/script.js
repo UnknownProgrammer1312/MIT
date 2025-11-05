4c1397078becf49a8e8dddc89d60005c3f6a05b6
ilarion <llarik.1999@gmail.com>
2025-11-06 00:13:59 +0200

    add lab2

--- MIT/lab2/task1lab/script.js
+++ MIT/lab2/task1lab/script.js
@@ -1,18 +1,96 @@
-const addHistoryButton = document.getElementById('addHistory');
-const deleteHistoryButton = document.getElementById('deleteHistory');
 
-const about = {url: "./about.html",
-            title: "About", 
-            decription: "About Page"
-};
+//Властивості window
+document.getElementById("windowsProperty").addEventListener('click',() => {
+    document.getElementById("widthInner").textContent = "Windows.innerWidth = " + window.innerWidth;
+    document.getElementById("heightInner").textContent = "Windows.innerHeight = " + window.innerHeight;
 
-const addRow = () => {
-    history.pushState(about, about.title, about.url);
-    document.title = about.title;
-    content.innerHTML = "<h1>About Page</h1><p>This is About page content</p>";
-
-};
-
-addHistoryButton.addEventListener('click', () => {
-addRow();
 });
+
+//Компоненти браузера
+document.getElementById("browserComponent").addEventListener('click',() => {
+    document.getElementById("srcBar").textContent = "Scrollbar is = " + window.scrollbars.visible;
+
+});
+
+// Властивість screen
+document.getElementById("screenProperty").addEventListener('click',() => {
+    const clr = screen.colorDepth;
+    document.getElementById("scrProp").textContent = "Color depth is = " + clr;
+
+});
+// Діалогові вікна
+document.getElementById("conf").addEventListener('click',() => {
+     const name = prompt("Enter your name");
+     const isItCorrect = confirm("Your name is " + name + "?")
+     if (isItCorrect === true) alert("Welcome " + name);
+     else alert("Try again");
+
+});
+
+// Відкриття, закриття та позиціонування вікон
+document.getElementById("windowsPosition").addEventListener('click',() => {
+     window.open("https://www.google.com/","_blank","popup=yes,width=480,height=480");
+});
+
+// Історія браузера. History API довжина 
+document.getElementById("length").addEventListener('click',() => {
+    document.getElementById("labelLenght").textContent = history.length;
+});
+// Історія браузера. History API додати 
+let count = 1
+document.getElementById("add").addEventListener('click',() => {
+    
+    history.pushState({page :"myPage"},"", `?ilarion=${count}`);
+    count++;
+    console.log(history.length);
+});
+// Історія браузера. History API повернутись
+document.getElementById("back").addEventListener('click',() => {
+    history.back();
+     
+});
+// Історія браузера. History API замінити
+document.getElementById("replace").addEventListener('click', () => {
+        
+        history.replaceState({ page: "myPage" }, "", `?ilarion=${--count}`);
+        console.log("History length після видалення:", history.length);
+        
+});
+// Об'єкт location. Управління адресою
+document.getElementById("location").addEventListener('click',() => {
+    console.log(location.host);
+    console.log(location.protocol);
+    console.log(location.port);
+    console.log(location.hostname);
+   console.log(navigator.appCodeName);
+});
+// Функція setTimeout
+document.getElementById("setTimeout").addEventListener('click', () => {
+  const timerId = setTimeout(() => {
+    for (let i = 0; i < 5; i++) {
+      console.log("Hello");
+    }
+  }, 5000);
+});
+// setInterval
+let intervalId = false; 
+document.getElementById("setInterval").addEventListener('click', () => {
+
+  if (intervalId) {
+    clearInterval(intervalId);
+  }
+
+  intervalId = setInterval(() => {
+    for (let i = 0; i < 5; i++) {
+      console.log("Hello");
+    }
+  }, 50);
+});
+
+// stopInterval
+document.getElementById("stopInterval").addEventListener('click', () => {
+  clearInterval(intervalId);
+  intervalId = true; 
+  console.log("Interval stopped");
+});
+
